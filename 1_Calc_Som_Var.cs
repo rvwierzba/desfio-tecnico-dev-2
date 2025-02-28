605dd0afdbb46839f6bdfa945070306118e48684
Rafa Wierzba <rafaelwierzba@gmail.com>
2025-02-28 14:54:00 -0300

    Create 1_Calc_Som_Var.cs

---
+++ 1_Calc_Som_Var.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Calc_Som_Var
+{
+
+/*
+
+  1)	Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0; 
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA); 
+Ao final do processamento, qual será o valor da variável SOMA? 
+    
+
+*/
+
+
+    public static void Main(string[] args)
+    {
+       int INDICE = 13, SOMA = 0, K = 0;
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+        Console.WriteLine(SOMA); // Saída: 91
+    }
+}
